42dfce9348a7a2ef53b0ea9bbfe2cd141105a38a
Ivan D.S. <ivandiazsanchez1996@gmail.com>
2024-10-11 17:02:39 +0200

    Especificacion 6 hecha

---
+++ game.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="ca">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="styles.css">
+    <title>Batalla Naval</title>
+</head>
+
+<body>
+    <h1>Batala Naval</h1>
+    <p>Enfonsar la flota o jugar a barcos és un joc de taula per a dues persones que es pot jugar amb llapis i paper o en versions de tauler. El seu objectiu és endevinar la situació dels vaixells de l'enemic i enfonsar-los indicant-ne les coordenades. Les primeres versions van sorgir a finals del segle xix però es va popularitzar poc després de la primera guerra mundial</p>
+
+    <button onclick="location.href='game.php'">Partida clàssica</button>
+    <button onclick="location.href='ranking.php'">Hall of Fame</button>
+</body>
+
+</html>
