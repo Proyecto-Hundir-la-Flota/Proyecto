c79c3d6ce505fc749d43f08df22956ac8a25209d
Victor <vvalerolopez.eb@iesesteveterradas.cat>
2024-10-14 19:29:17 +0200

    Wireframes arreglados, look & feel hecho y landing page

---
+++ game.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
+    <script src="https://kit.fontawesome.com/9c44094610.js" crossorigin="anonymous"></script>
+</head>
+<body>
+    <h1>Excavació Juràssica</h1>
+    <button class="nav-button home-button"><i class="fa-solid fa-chevron-left icon"></i>Inici</button>
+    <button class="nav-button rank-button">Rànquing<i class="fa-solid fa-chevron-right icon"></i></button>
+</body>
+</html>
--- index.php
+++ index.php
@@ -1,26 +1,21 @@
 <!DOCTYPE html>
-<html lang="ca">
-
+<html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" type="text/css" href="style.css">
-    <title>Batalla Naval</title>
+    <title>Excavació Jurásica</title>
+    <link rel="stylesheet" href="style.css">
+    <script src="https://kit.fontawesome.com/9c44094610.js" crossorigin="anonymous"></script>
 </head>
-
-<body>
-    <div class="container">
-        <div class="content">
-            <h1>Batala Naval</h1>
-            <p>Enfonsar la flota o jugar a barcos és un joc de taula per a dues persones que es pot jugar amb llapis i paper o en versions de tauler. El seu objectiu és endevinar la situació dels vaixells de l'enemic i enfonsar-los indicant-ne les coordenades. Les primeres versions van sorgir a finals del segle xix però es va popularitzar poc després de la primera guerra mundial</p>
-
-            <button onclick="location.href='game.php'">Partida clàssica</button>
-            <button onclick="location.href='ranking.php'">Hall of Fame</button>
-        </div>
-        <div class="shipImage">
-            <img src="ship.png" alt="Nau Espacial">
+<body id="landing_page"> 
+    <div class="hero">
+        <h1>Excavació Jurásica</h1>
+        <h3>Desenterra ossos de fa milions d'anys!</h3>
+        <p>Explora i excava per trobar ossos de dinosaures ocults sota terra. Tria les coordenades correctes i descobreix tresors prehistòrics. Seràs capaç de desenterrar-los tots?</p>
+            <div class="button_container">    
+            <button><i class="fa-solid fa-gamepad"></i>Començar Joc</button>
+            <button><i class="fa-solid fa-ranking-star"></i>Rànquing Paleontòlegs</button>
         </div>
     </div>
 </body>
-
 </html>
