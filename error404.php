5fd443f9645ecb5d2509bd330ecbbec41d95f11c
Ivan D.S. <ivandiazsanchez1996@gmail.com>
2024-10-25 18:01:01 +0200

    Error 404 para servidor externo del Proyecto

---
+++ error404.php
@@ -0,0 +1,21 @@
+<?php header("HTTP/1.0 404 Not Found"); ?>
+
+<!DOCTYPE html>
+<html lang='ca'>
+
+<head>
+    <meta charset='UTF-8'>
+    <meta name='viewport' content='width=device-width, initial-scale=1.0'>
+    <link rel='stylesheet' href='style.css'>
+    <script src='https://kit.fontawesome.com/9c44094610.js' crossorigin='anonymous'></script>
+    <title>Error 404 - Excavació Juràssica</title>
+</head>
+
+<body class='not-found-page'>
+    <div class='hero'>
+        <h1>Error 404 - Not Found</h1>
+        <p>La pàgina que estàs buscant no existeix. Si creus que això és un error, contacta amb l'administrador del lloc web.</p>
+    </div>
+</body>
+
+</html>;
